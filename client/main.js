44a341a6f8a9da67dbada634ef7b109776341bbc
Syed Aman <sna274@nyu.edu>
2016-12-04 19:37:33 -0500

    add todosListModule to application

--- client/main.js
+++ client/main.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
 
 // create angular module
 angular.module('simple-todos', [
-  angularMeteor
+  angularMeteor,
+  todosList.name
 ]);
